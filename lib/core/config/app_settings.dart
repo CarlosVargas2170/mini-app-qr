d0e6df36bd53b88862cdc7adaad9afda67aa372e
CVargas2170 <vargascarlos2170@gmail.com>
2026-07-28 10:09:52 -0400

    [FIX]-update values in appSettings

--- lib/core/config/app_settings.dart
+++ lib/core/config/app_settings.dart
@@ -24,8 +24,7 @@ class AppSettings {
 
   /// Configuracion de visibilidad de merchants y productos.
   ProductFilterConfig filterConfig = ProductFilterConfig(
-    pinnedProducts: {489150, 489161},
-    filterMode: 'whitelist',
+    filterMode: 'all',
   );
 
   /// Compatibilidad temporal: retorna el primer merchantId de la lista.
@@ -33,7 +32,7 @@ class AppSettings {
   int get merchantId => merchantIds.isNotEmpty ? merchantIds.first : 0;
 
   /// Nombre del cliente usado al crear órdenes.
-  static String customerName = 'Robot Mesero';
+  static String customerName = 'Mesero-test-prod';
 
   bool get isConfigured =>
       baseUrl.isNotEmpty &&
